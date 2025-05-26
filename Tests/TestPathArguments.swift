609a75aac5ecf612bc705490c555da8a9333be04
Sean Coates <sean@seancoates.com>
2025-05-26 03:50:52 +0000

    improve illustration of path:resolvedPath

--- Tests/TestPathArguments.swift
+++ Tests/TestPathArguments.swift
@@ -34,6 +34,7 @@ struct MacroRoutingTestPathArguments {
             ) == "/movie/ratatouille/2007"
         )
 
+        #expect(Controller.$Routing.other.path == "/mixed/{one}/{two}/{three}/{four}/{five}")
         #expect(
             Controller.$Routing.other.resolvedPath(
                 one: "apple", two: "banana", three: "carrot",
@@ -41,7 +42,7 @@ struct MacroRoutingTestPathArguments {
             ) == "/other/apple/banana/carrot/durian/eggplant"
         )
 
-        // @GET("/mixed/{one}/:two/{three}/:four/{five}")
+        #expect(Controller.$Routing.mixed.path == "/mixed/{one}/:two/{three}/:four/{five}")
         #expect(
             Controller.$Routing.mixed.resolvedPath(
                 one: "apple", two: "banana", three: "carrot",
